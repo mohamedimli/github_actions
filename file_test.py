f1b5e742b105d111dbd1cd2fd787d9602d7dae79
Mohamed <mohamedimli@gmail.com>
2024-12-28 17:45:04 +0100

    Add unit tests for basic arithmetic operations and string comparison

---
+++ file_test.py
@@ -0,0 +1,21 @@
+import pytest
+
+def test_calc_addition():
+  # Function to test the result of 2+4
+    output = 2+4
+    assert output == 6
+
+def test_calc_substraction():
+  # Function to test the result of 2-4
+    output = 2-4
+    assert output == -2
+
+def test_calc_multiply():
+  # Function to test the result of 2*4
+    output = 2*4
+    assert output == 8
+
+def test_coucou():
+  # Function to test if the result returns 'hello'
+    output = 'hello'
+    assert output == 'hello'
